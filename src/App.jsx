9de0fc0633e54410090d032e7930d69b0ca26d4b
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2025-07-28 22:13:49 +0500

    project initalized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <h1 className='text-red-500 text-8xl font-[Jost] text-center mt-50'>Hello</h1>
+  </div>
+  )
+}
+
+export default App
