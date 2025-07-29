3cfc24242c988066e3dbeaaebdfa569f93422327
Muzaffar Hussain <muzaffarhussain0055@gmail.com>
2025-07-29 23:21:57 +0500

    pages/reservation completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-  import Footer from "./components/Footer";
-  import { Route, Routes } from "react-router-dom";
-  import GeneralLayout from "./layout/GeneralLayout";
-  import Hometwo from "./pages/Hometwo";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import GeneralLayout from "./layout/GeneralLayout";
+import Hometwo from "./pages/Hometwo";
 import Hometwolayout from "./layout/Hometwolayout";
 import Homethreelayout from "./layout/Homethreelayout";
 import Homethree from "./pages/Homethree";
 import Homeone from "./pages/Homeone";
+import Reservation from ".pages/Reservation.jsx"
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* Home-two Layout */}
+        <Route element={<Hometwolayout />}>
+          <Route path="/" element={<Hometwo />} />
+        </Route>
 
-  function App() {
-    return (
-      <div className="App">
-        <Routes>
-          
-          {/* Home-two Layout */}
-          <Route element={<Hometwolayout />}>
-            <Route path="/" element={<Hometwo />} />
-          </Route>
+        {/* General Layout */}
+        <Route element={<GeneralLayout />}>
+          <Route path="/home-1" element={<Homeone />} />
+          <Route path="/reservation" element={<Reservation />} />
+        </Route>
 
-          {/* General Layout */}
-          <Route element={<GeneralLayout />}>
-            <Route path="/home-1" element={<Homeone />} />
-          </Route>
+        {/* home-three Layout */}
+        <Route element={<Homethreelayout />}>
+          <Route path="/home-3" element={<Homethree />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
 
-          {/* home-three Layout */}
-          <Route element={<Homethreelayout />}> 
-            <Route path="/home-3" element={<Homethree  />} />
-          </Route>
-
-        </Routes>
-        <Footer />
-      </div>
-    );
-  }
-
-  export default App;
+export default App;
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import imgUrl from "../assets/bg.jpg";
+const Blog = () => {
+  return (
+    <div>Blog</div>
+  )
+}
+
+export default Blog
