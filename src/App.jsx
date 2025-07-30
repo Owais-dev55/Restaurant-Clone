ed6dc878ade218dc5d48e0fa71d73f85bc3b9e79
Urooj Rafique <145989623+Uroojrafique5@users.noreply.github.com>
2025-07-30 15:58:26 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 // import Headerone from "./components/Headerone";
-import Headertwo from "./components/Headertwo";
+// import Headertwo from "./components/Headertwo";
 // import Headerthree from "./components/Headerthree";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
+import HomeThree from "./pages/HomeThree";
 
 
 function App() {
   return (
     <div className="App">
       {/* <Headerone /> */}
-      <Headertwo/>
+      {/* <Headertwo/> */}
       {/* <Headerthree/> */}
-      <Footer/>
       {/* Add more sections here like Hero, About, Menu, etc. */}
+      <HomeThree/>
+      {/* <Footer/> */}
     </div>
+    
   );
+  
 }
 
 export default App;
