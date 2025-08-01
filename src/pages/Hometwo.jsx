ca20b77a9ceea8f15d38efcb97c30735ac05e3e2
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2025-08-01 14:51:12 +0500

    Updated Home

--- src/pages/Hometwo.jsx
+++ src/pages/Hometwo.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import Reservation from '../components/HomeTwo/Reservation'
-import Testimonial from '../components/HomeTwo/testimonial'
+import Testimonial from '../components/HomeTwo/Testimonial'
 import Hero from '../components/HomeTwo/Hero'
 import FoodSection from '../components/HomeTwo/second'
 import Discovery from '../components/HomeTwo/Discovery'
